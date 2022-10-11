ae6474584d966bac62720826092d549ab2163918
ZmMatvey <115418268+ZmMatvey@users.noreply.github.com>
2022-10-11 20:31:48 +0300

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,59 +1,59 @@
 class Matrix
 {
 private:
-unsigned int m; //количество вектор строк
+unsigned int m; //number of vector strings
 
-unsigned int n; //количество вектор столбцов
+unsigned int n; //number of vector columns
 
 public:
-Vector* const V; //указатель на массив вектор строк
+Vector* const V; //array of vector strings (Vector)
 
-Matrix (unsigned int a, unsigned int b);//создаЄт матрицу axb
+Matrix (unsigned int m, unsigned int n); //creates an mxn matrix
 
-Matrix(const Matrix& B);//правило Ѕольшой тройки-1 конструктор копировани€
+Matrix(const Matrix& B); //rule of three-1 copy constructor
 
 unsigned int GetM_m();
 
 unsigned int GetM_n();
 
-void SetM_fill(); //заполнение матрицы вручную
+void SetM_fill(); //filling in the matrix manually
 
-void SetM_fill(Vector* X); //заполнение матрицы при помощи массива векторов
+void SetM_fill(Vector* X); //filling the matrix with an array of vectors
 
-void Cout_Matrix(); //вывод матрицы
+void Cout_Matrix(); //matrix output (not finalized, since it outputs lines with a different number of characters crookedly)
 
-void stringij(unsigned int i, unsigned int j); //мен€ет местами i, j строки
+void stringij(unsigned int i, unsigned int j); //swaps i, j string
 
-void columnij(unsigned int i, unsigned int j); //мен€ет местами i, j столбцы
+void columnij(unsigned int i, unsigned int j); //swaps i, j column
 
-Matrix T(); //транспонированна€ матрица
+Matrix T(); //transposed matrix
+  
+Matrix inverseA(); //inverse matrix
 
-Matrix inverseA();//обратна€ матрица
+//int* p(); //positive and negative inertia indices
 
-//int* p(); //положительный и отрицательный индексы инерции
+double xi(double x); //the characteristic polynomial of x
 
-double xi(double x); //характеристический многочлен от x
-
-Matrix& operator= (const Matrix& B); //правило Ѕольшой тройки-2 оператор присваивани€
-
-~Matrix ();//правило Ѕольшой тройки-3 деструктор
+Matrix& operator= (const Matrix& B); //rule of three-2 assignment operator
+  
+~Matrix (); //rule of three-3 destructor
 };
 
-Matrix E(unsigned int n); //единична€ матрица размера n
-Matrix O(unsigned int m, unsigned int n);//нулева€ матрица размерами mxn
-double det(Matrix A); //определитель матрицы A
-double tr(Matrix A); //след матрицы A
-//int rg(Matrix A); //ранг матрицы A
-//Matrix FDS(Matrix A);//‘—– линейного матричного уравнени€
-//Matrix EXP(Matrix A); //матрична€ экспонента
+Matrix E(unsigned int n); //a unit matrix of size nxn
+Matrix O(unsigned int m, unsigned int n); //zero matrix with dimensions mxn
+double det(Matrix A); //determinant of the matrix A
+double tr(Matrix A); //the trace of the matrix A
+//int rg(Matrix A); //matrix rank A
+//Matrix SOL(Matrix A);//solution of a linear matrix equation of the form A*x=b
+//Matrix EXP(Matrix A); //matrix exponent
 Matrix operator+ (Matrix A, Matrix B); //A+B
 Matrix operator- (Matrix A, Matrix B); //A-B
 bool operator== (Matrix A, Matrix B); //A == B
-Matrix operator* (double a, Matrix B); //умножение матрицы b на скал€р а
-Vector operator* (Vector a, Matrix B); //умножение вектор строки на матрицу
-Vector operator* (Matrix A, Vector b); //умножение матрицы на вектор столбец
+Matrix operator* (double a, Matrix B); //a*B
+Vector operator* (Vector a, Matrix B); //multiplying a string vector by a matrix
+Vector operator* (Matrix A, Vector b); //multiplying a matrix by a column vector
 Matrix operator* (Matrix A, Matrix B); //A*B
 Matrix operator/ (Matrix B, double a); //B/a
-//Matrix operator[] (Matrix A, Matrix B);//[A,B] = A*B-B*A
-//Matrix operator^ (Matrix A, int n); //A^n возведение матрицы в степень
+//Matrix operator[] (Matrix A, Matrix B); //[A,B] = A*B-B*A
+//Matrix operator^ (Matrix A, int n); //A^n exponentiation of the matrix
 
