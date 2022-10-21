dd407498551e821f7faaa50434558183a52d3aa5
ZmMatvey <115418268+ZmMatvey@users.noreply.github.com>
2022-10-21 19:57:05 +0300

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -1,98 +1,106 @@
 #include <iostream>
+#include <assert.h>
 #include "Vector.h"
 
-using namespace std;
+Vector::Vector (): v(new double[3]), N(3) {}
 
-Vector::Vector (): v(new double[3]), N(3) {} //проблема в создании матрицы - число столбцов фиксировано и равно 3
-
-Vector::Vector (unsigned int a): v(new double[a]), N(a) {}
+Vector::Vector (int a): v(new double[a]), N(a) {}
 
 Vector::Vector(const Vector& b): Vector(b.N) {
-    for (unsigned int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) {
         v[i] = b.v[i];
     }
 }
 
-unsigned int Vector::Get_N() {
+Vector::Vector(Vector&& b): N(b.N), v(b.v) {}
+
+int Vector::Get_N() {
     return N;
 }
 
 void Vector::Set_fill() {
-    for (unsigned int i = 0; i < N; i++) {
-        cin>>v[i];
+    for (int i = 0; i < N; i++) {
+        std::cin>>v[i];
     }
 }
 
 void Vector::Set_fill(double* x) {
-    for (unsigned int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) {
         v[i] = x[i];
     }
 }
 
 void Vector::Cout_Vector() {
-    cout<<"||";
-    for (unsigned int i = 0; i < N-1; i++) {
-        cout<<v[i]<<" ";
+    std::cout<<"||";
+    for (int i = 0; i < N-1; i++) {
+        std::cout<<v[i]<<" ";
     }
-    cout<<v[N-1];
-    cout<<"||"<<endl;
+    std::cout<<v[N-1];
+    std::cout<<"||"<<std::endl;
 }
 
 Vector& Vector::operator= (const Vector& b) {
-    if (N == b.N) {
-        if (this == &b) {
-            return *this;
-        }
-    for (unsigned int i = 0; i < N; i++) {
+    assert (N == b.N);
+    if (this == &b) {
+        return *this;
+    }
+    for (int i = 0; i < N; i++) {
         v[i] = b.v[i];
     }
     return *this;
-    }
+}
+
+Vector& Vector::operator= (Vector&& b) {
+    this->N = b.N;
+    this->v = b.v;
+    return *this;
 }
 
 Vector::~Vector () {
     delete []v;
 }
 
-Vector o(unsigned int a) {
+double& Vector::operator[](int i) {
+    return v[i];
+}
+
+Vector o(int a) {
     Vector c = Vector(a);
-    for (unsigned int i = 0; i < a; i++) {
-        c.v[i] = 0;
+    for (int i = 0; i < a; i++) {
+        c[i] = 0;
     }
     return c;
 }
 
 Vector operator+ (Vector a, Vector b){
-    unsigned int x = a.Get_N();
-    unsigned int y = b.Get_N();
-    if (x == y) {
-        Vector c = Vector(x);
-        for (unsigned int i = 0; i < x; i++) {
-            c.v[i] = a.v[i] + b.v[i];
-        }
-    return c;
+    int x = a.Get_N();
+    int y = b.Get_N();
+    assert (x == y);
+    Vector c = Vector(x);
+    for (int i = 0; i < x; i++) {
+        c[i] = a[i] + b[i];
     }
+    return c;
 }
 
 Vector operator- (Vector a, Vector b) {
-    unsigned int x = a.Get_N();
-    unsigned int y = b.Get_N();
-    if (x == y) {
-        Vector c = Vector(x);
-        for (unsigned int i = 0; i < x; i++) {
-            c.v[i] = a.v[i] - b.v[i];
-        }
-    return c;
+    int x = a.Get_N();
+    int y = b.Get_N();
+    assert (x == y);
+    Vector c = Vector(x);
+    for (int i = 0; i < x; i++) {
+        c[i] = a[i] - b[i];
     }
+    return c;
 }
 
 bool operator== (Vector a, Vector b) {
-    unsigned int x = a.Get_N();
-    unsigned int y = b.Get_N();
+    int x = a.Get_N();
+    int y = b.Get_N();
     if (x == y) {
         bool q = true;
-        for (unsigned int i = 0; i < x; i++) {
-            q = q && (a.v[i] == b.v[i]);
+        for (int i = 0; i < x; i++) {
+            q = q && (a[i] == b[i]);
         }
         return q;
     }
@@ -100,10 +108,10 @@ bool operator== (Vector a, Vector b) {
 }
 
 Vector operator* (double a, Vector b) {
-    unsigned int x = b.Get_N();
+    int x = b.Get_N();
     Vector c = Vector(x);
-    for (unsigned int i = 0; i < x; i++) {
-        c.v[i] = a*b.v[i];
+    for (int i = 0; i < x; i++) {
+        c[i] = a*b[i];
     }
     return c;
 }
