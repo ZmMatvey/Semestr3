24c7fec100a6773a1a56784a211fa4ecb799895e
ZmMatvey <115418268+ZmMatvey@users.noreply.github.com>
2022-10-21 20:04:04 +0300

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -1,34 +1,45 @@
 class Vector
 {
 protected:
-unsigned int N; //vector size
+int N; //vector size
+
+double* v;
 
 public:
-double* const v; //double array
 
-Vector();  //not finalized, not consistent with the matrix class
+Vector();
 
-Vector(const Vector& b); //rule of three 1-copy constructor
+Vector(const Vector& b); //rule of five 1-copy constructor
 
-Vector (unsigned int a); //creates a vector from R^a
+Vector(Vector&& b); //rule of five 2-move constructor
+  
+Vector (int a); //creates a vector from R^a
 
-unsigned int Get_N();
+int Get_N();
 
 void Set_fill(); //filling in the vector manually
 
 void Set_fill(double* x); //filling a vector with an array
 
-void Cout_Vector(); //vector output
+void Cout_Vector();
 
-Vector& operator= (const Vector& b); //rule of three 2-assignment operator
+Vector& operator= (const Vector& b); //rule of five 3-assignment operator
 
-~Vector (); //rule of three 3-destructor
+Vector& operator= (Vector&& b); //rule of five 4-move operator
 
+double& operator[] (int i);
+
+~Vector (); //rule of five 5-destructor
 };
 
-Vector o(unsigned int a); //zero vector
+Vector o(int a); //zero vector
+
 Vector operator+ (Vector a, Vector b); //a+b
+
 Vector operator- (Vector a, Vector b); //a-b
+
 bool operator== (Vector a, Vector b); //a == b
+
 Vector operator* (double a, Vector b); //a*b
+
 Vector operator/ (Vector b, double a); //b/a
