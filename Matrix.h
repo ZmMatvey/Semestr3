d88b3e0e39b27638833fba9c334fe560689d3423
ZmMatvey <115418268+ZmMatvey@users.noreply.github.com>
2022-10-21 19:56:03 +0300

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,56 +1,80 @@
+#include "Vector.h"
+
 class Matrix
 {
-private:
-unsigned int m; //number of vector strings
+protected:
+int m; //number of strings
 
-unsigned int n; //number of vector columns
+int n; //number of columns
+
+Vector* V; //pointer to an array of vector strings
 
 public:
-Vector* const V; //array of vector strings (Vector)
 
-Matrix (unsigned int m, unsigned int n); //creates an mxn matrix
+Matrix (int m, int n); //creates a matrix with m - strings, n - columns
 
-Matrix(const Matrix& B); //rule of three-1 copy constructor
+Matrix(const Matrix& B); //rule of five - 1 copy constructor
 
-unsigned int GetM_m();
+Matrix(Matrix&& B); //rule of five - 2 move constructor
 
-unsigned int GetM_n();
+int GetM_m();
+
+int GetM_n();
 
 void SetM_fill(); //filling in the matrix manually
 
 void SetM_fill(Vector* X); //filling the matrix with an array of vectors
 
-void Cout_Matrix(); //matrix output (not finalized, since it outputs lines with a different number of characters crookedly)
+void Cout_Matrix(); 
 
-void stringij(unsigned int i, unsigned int j); //swaps i, j string
+void stringij(int i, int j); //swaps i, j string
 
-void columnij(unsigned int i, unsigned int j); //swaps i, j column
+void columnij(int i, int j); //swaps i, j columns element by element
+//therefore, it is better to use stringij whenever possible, the code will be faster
 
 Matrix T(); //transposed matrix
-  
-Matrix inverse(); //inverse matrix
 
-//int* p(); //positive and negative inertia indices
+Matrix inverse(); //inverse matrix
 
 double xi(double x); //the characteristic polynomial of x
 
-Matrix& operator= (const Matrix& B); //rule of three-2 assignment operator
-  
-~Matrix (); //rule of three-3 destructor
+Matrix& operator= (const Matrix& B); //rule of five - 3 assignment operator
+
+Matrix& operator= (Matrix&& B); // rule of five - 4 move operator
+
+Vector& operator[] (int i);
+
+~Matrix (); //rule of five - 5 destructor
+
 };
 
-Matrix E(unsigned int n); //a unit matrix of size nxn
-Matrix O(unsigned int m, unsigned int n); //zero matrix with dimensions mxn
-double tr(Matrix A); //the trace of the matrix A
-double det(Matrix A); //determinant of the matrix A
-int rg(Matrix A); //matrix rank A
-//Matrix SOL(Matrix A);//solution of a linear matrix equation of the form A*x=b
+Matrix E(int n); 
+
+Matrix O(int m, int n);
+
+double tr(Matrix A);
+
+double det(Matrix A);
+
+int rg(Matrix A);
+
+Matrix SOL(Matrix A, Vector b); //solutions of a linear matrix equation of the form A*x=b 
+//returns the matrix 1 column - a partial solution, the rest - the basis of solutions of a homogeneous equation
+
 Matrix operator+ (Matrix A, Matrix B); //A+B
+
 Matrix operator- (Matrix A, Matrix B); //A-B
+
 bool operator== (Matrix A, Matrix B); //A == B
-Matrix operator* (double a, Matrix B); //a*B
-Vector operator* (Vector a, Matrix B); //multiplying a string vector by a matrix
+
+Matrix operator* (double a, Matrix B); //multiplication of matrix b by scalar a
+
+Vector operator* (Vector a, Matrix B); //multiplying a matrix by a string vector
+
 Vector operator* (Matrix A, Vector b); //multiplying a matrix by a column vector
-Matrix operator* (Matrix A, Matrix B); //A*B
-Matrix operator/ (Matrix B, double a); //B/a
+
+Matrix operator* (Matrix A, Matrix B); // A*B
+
+Matrix operator/ (Matrix B, double a); // B/a
+
 Matrix operator^ (Matrix A, int n); //A^n exponentiation of the matrix
