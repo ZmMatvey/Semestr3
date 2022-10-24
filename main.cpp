ef0ef667e9d5fe5bc000a9541d594f8bf6729499
ZmMatvey <115418268+ZmMatvey@users.noreply.github.com>
2022-10-24 10:53:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include "Matrix.h"
+using namespace std;
 
 int main()
 {
+    Matrix A = Matrix(4, 2);
+    A.set_fill();
+    Matrix B = Matrix(2, 4);
+    B.set_fill();
+    Matrix C = A*B; //rg(C) <= min(rg(A);rg(B))< 4 -> det(C) = 0;
+    cout<<det(C)<<endl;
+    cout<<rg(C);
     return 0;
 }
